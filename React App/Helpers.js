651b23db032b79ad9f441f82b9b21a20e87e33ea
abdullahalshahin <mdabdullahalshahin@gmail.com>
2024-02-13 11:05:23 +0600

    add react new file for update Head

---
+++ React App/Helpers.js
@@ -0,0 +1,30 @@
+export class Helpers {
+    static updateHeadComponentDidMount(title, metaDescription) {
+		document.title = title;
+	
+		const descriptionMeta = document.querySelector('meta[name="description"]');
+
+		if (descriptionMeta) {
+			descriptionMeta.content = metaDescription;
+		} else {
+			const newDescriptionMeta = document.createElement('meta');
+
+			newDescriptionMeta.name = 'description';
+			newDescriptionMeta.content = metaDescription;
+
+			document.head.appendChild(newDescriptionMeta);
+		}
+    }
+
+    static updateHeadComponentWillUnmount() {
+		document.title = 'React App';
+	
+		const descriptionMeta = document.querySelector('meta[name="description"]');
+
+		if (descriptionMeta) {
+			descriptionMeta.content = 'React App';
+		}
+    }
+};
+
+export default Helpers;
